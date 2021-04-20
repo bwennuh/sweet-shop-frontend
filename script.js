ed68ef82ecb1d08aa2ce5084d312096ec5a8d342
bwennuh <brennabruening@gmail.com>
2021-04-20 09:31:45 -0400

    Add fetch GET request function

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 // --- Write the code for the Sweet Shop below! ---
+
+const dessertsURL = "http://localhost:3000/desserts/"
+
+const recURL = "http://localhost:3000/recommendations/"
+
+
+//* Functions to run at start
+getDesserts();
+
+
+
+//TODO Functions we'll want to include
+
+function getDesserts() {
+  fetch(dessertsURL)
+  .then(resp => resp.json())
+  // .then(data => console.log(data))
+  .then(data => data.forEach(renderDesserts))
+}
+
+function renderDesserts(dessert) {
+  
+
+
+}
